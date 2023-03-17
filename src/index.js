1eb126ebc6dfe7303940db95cd084ae981b7a86d
Babytskyi-Kolya <112752847+Babytskyi-Kolya@users.noreply.github.com>
2023-03-17 18:27:00 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -154,11 +154,11 @@ function buttonShow() {
 }
 
 function smoothScroll() {
-  const { height: cardHeight } = document
-    .querySelector('.photo-card')
-    .firstElementChild.getBoundingClientRect();
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-}
+    const { height: cardHeight } = document
+      .querySelector('.photo-card')
+      .firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
