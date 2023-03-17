708b9db284a85a299879a0c41c3bac776b12a78d
Babytskyi-Kolya <112752847+Babytskyi-Kolya@users.noreply.github.com>
2023-03-17 18:22:44 +0300

    first-commit

--- src/js/search-service.js
+++ src/js/search-service.js
@@ -2,3 +2,41 @@ import axios from 'axios';
 
 const API_KEY = '18198238-415cec3f9ecdd7367e0980e97';
 const URL = 'https://pixabay.com/api/';
+
+export default class SearchApiService {
+    constructor() {
+      this.searchQuery = '';
+      this.page = 1;
+    }
+  
+    async fetchImages() {
+      try {
+        return await axios.get(URL, {
+          params: {
+            key: API_KEY,
+            q: this.searchQuery,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'true',
+            page: this.page,
+            per_page: 40,
+          },
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  
+    resetPage() {
+      this.page = 1;
+    }
+  
+    get query() {
+      return this.searchQuery;
+    }
+  
+    set query(newQuery) {
+      this.searchQuery = newQuery;
+    }
+  }
